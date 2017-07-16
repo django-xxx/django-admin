e5a45a1802a4dd353820a8fe3c128244e70fc1b0
Brightcells <brightcells@gmail.com>
2017-07-16 18:49:23 +0800

    :sparkles: Add all fields readonly class ReadonlyModelAdmin

--- djadmin/admin.py
+++ djadmin/admin.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 
-admin.site.disable_action('delete_selected')
+if not hasattr(settings, 'DISABLE_ACTION') or settings.DISABLE_ACTION:
+    admin.site.disable_action('delete_selected')
 
 
 class DeleteModelAdmin():
@@ -15,3 +17,10 @@ class DeleteModelAdmin():
             self.delete_model(request, o)
 
     override_delete_selected.short_description = _(u'Delete selected %(verbose_name_plural)s')
+
+
+class ReadonlyModelAdmin():
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return self.readonly_fields + tuple(f.name for f in self.model._meta.fields)
+        return self.readonly_fields
