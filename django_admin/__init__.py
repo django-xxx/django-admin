ec23b443e838102e539aa13289c1fb081916fed8
Brightcells <brightcells@gmail.com>
2018-07-17 13:48:18 +0800

    :bug: Fix Bug: Import SpecifiedQuantityQuerySetModelAdmin error

--- django_admin/__init__.py
+++ django_admin/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from django_admin.djadmin import DeleteModelAdmin, ReadonlyModelAdmin, Readonly2ModelAdmin, ExportExcelModelAdmin, AdvancedExportExcelModelAdmin, ReadOnlyModelAdmin, ChangeOnlyModelAdmin, DeleteonlyModelAdmin, DeleteOnlyModelAdmin, AddOnlyModelAdmin, AdvancedActionsModelAdmin
+from django_admin.djadmin import DeleteModelAdmin, ReadonlyModelAdmin, Readonly2ModelAdmin, ExportExcelModelAdmin, AdvancedExportExcelModelAdmin, ReadOnlyModelAdmin, ChangeOnlyModelAdmin, DeleteonlyModelAdmin, DeleteOnlyModelAdmin, AddOnlyModelAdmin, AdvancedActionsModelAdmin, SpecifiedQuantityQuerySetModelAdmin
